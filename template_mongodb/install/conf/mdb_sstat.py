2d82590e2c1d3000c40c999f2f35242a1fa8fae6
meetbill <meetbill@163.com>
2017-09-11 17:53:57 +0800

    update:更新 mongodb check

--- template_mongodb/install/conf/mdb_sstat.py
+++ template_mongodb/install/conf/mdb_sstat.py
@@ -4,11 +4,10 @@
 # Author: Bill
 # Created Time : 2016-07-27 23:18:24
 
-# File Name: www.py
+# File Name: mdb_sstat.py
 # Description:
 
 """
-import commands
 import sys
 import os
 from optparse import OptionParser
@@ -16,12 +15,129 @@ import re
 import time
 import platform
 import json
-import pprint
 import types
+#######Log
+import logging
+from logging.handlers import RotatingFileHandler
+class ColoredFormatter(logging.Formatter):
+    '''A colorful formatter.'''
+ 
+    def __init__(self, fmt = None, datefmt = None):
+        logging.Formatter.__init__(self, fmt, datefmt)
+        # Color escape string
+        COLOR_RED='\033[1;31m'
+        COLOR_GREEN='\033[1;32m'
+        COLOR_YELLOW='\033[1;33m'
+        COLOR_BLUE='\033[1;34m'
+        COLOR_PURPLE='\033[1;35m'
+        COLOR_CYAN='\033[1;36m'
+        COLOR_GRAY='\033[1;37m'
+        COLOR_WHITE='\033[1;38m'
+        COLOR_RESET='\033[1;0m'
+         
+        # Define log color
+        self.LOG_COLORS = {
+            'DEBUG': '%s',
+            'INFO': COLOR_GREEN + '%s' + COLOR_RESET,
+            'WARNING': COLOR_YELLOW + '%s' + COLOR_RESET,
+            'ERROR': COLOR_RED + '%s' + COLOR_RESET,
+            'CRITICAL': COLOR_RED + '%s' + COLOR_RESET,
+            'EXCEPTION': COLOR_RED + '%s' + COLOR_RESET,
+        }
 
-from qiueer.python.QLog import Log
+    def format(self, record):
+        level_name = record.levelname
+        msg = logging.Formatter.format(self, record)
+ 
+        return self.LOG_COLORS.get(level_name, '%s') % msg
+class Log(object):
+    
+    '''
+    log
+    '''
+    def __init__(self, filename, level="debug", logid="qiueer", mbs=20, count=10, is_console=True):
+        '''
+        mbs: how many MB
+        count: the count of remain
+        '''
+        try:
+            self._level = level
+            #print "init,level:",level,"\t","get_map_level:",self._level
+            self._filename = filename
+            self._logid = logid
 
-#{{{docmd
+            self._logger = logging.getLogger(self._logid)
+            
+            
+            if not len(self._logger.handlers):
+                self._logger.setLevel(self.get_map_level(self._level))  
+                
+                fmt = '[%(asctime)s] %(levelname)s\n%(message)s'
+                datefmt = '%Y-%m-%d %H:%M:%S'
+                formatter = logging.Formatter(fmt, datefmt)
+                
+                maxBytes = int(mbs) * 1024 * 1024
+                file_handler = RotatingFileHandler(self._filename, mode='a',maxBytes=maxBytes,backupCount=count)
+                self._logger.setLevel(self.get_map_level(self._level))  
+                file_handler.setFormatter(formatter)  
+                self._logger.addHandler(file_handler)
+    
+                if is_console == True:
+                    stream_handler = logging.StreamHandler(sys.stderr)
+                    console_formatter = ColoredFormatter(fmt, datefmt)
+                    stream_handler.setFormatter(console_formatter)
+                    self._logger.addHandler(stream_handler)
+
+        except Exception as expt:
+            print expt
+            
+    def tolog(self, msg, level=None):
+        try:
+            level = level if level else self._level
+            level = str(level).lower()
+            level = self.get_map_level(level)
+            if level == logging.DEBUG:
+                self._logger.debug(msg)
+            if level == logging.INFO:
+                self._logger.info(msg)
+            if level == logging.WARN:
+                self._logger.warn(msg)
+            if level == logging.ERROR:
+                self._logger.error(msg)
+            if level == logging.CRITICAL:
+                self._logger.critical(msg)
+        except Exception as expt:
+            print expt
+            
+    def debug(self,msg):
+        self.tolog(msg, level="debug")
+        
+    def info(self,msg):
+        self.tolog(msg, level="info")
+        
+    def warn(self,msg):
+        self.tolog(msg, level="warn")
+        
+    def error(self,msg):
+        self.tolog(msg, level="error")
+        
+    def critical(self,msg):
+        self.tolog(msg, level="critical")
+            
+    def get_map_level(self,level="debug"):
+        level = str(level).lower()
+        #print "get_map_level:",level
+        if level == "debug":
+            return logging.DEBUG
+        if level == "info":
+            return logging.INFO
+        if level == "warn":
+            return logging.WARN
+        if level == "error":
+            return logging.ERROR
+        if level == "critical":
+            return logging.CRITICAL
+
 def docmd(command,timeout=300, raw=False):
         '''
         功能：
@@ -56,18 +172,13 @@ def docmd(command,timeout=300, raw=False):
         
         if not ps.returncode:
                 retcode = ps.returncode
-        
         if raw == True:  #去除行末换行符
                 stdo = [line.strip("\n") for line in stdo]
                 stde = [line.strip("\n") for line in stde]
-        
         if raw == False: #去除行末换行符，制表符、空格等
                 stdo = [str.strip(line) for line in stdo]
                 stde = [str.strip(line) for line in stde]
-
         return (stdo,stde,retcode)
-#}}}
-#{{{get_logstr
 def get_logstr(list_dict, max_key_len=16, join_str="\n"):
     log_str = ""
     for conf in list_dict:
@@ -75,8 +186,6 @@ def get_logstr(list_dict, max_key_len=16, join_str="\n"):
             log_str = log_str + str(key).ljust(max_key_len) + ": " + str(val) + join_str
     log_str = log_str.strip() # 去掉尾部 \n
     return log_str
-#}}}
-#{{{get_user_passwd_by_port
 def get_user_passwd_by_port(conffile, port):
     if os.path.exists(conffile) == False:
         return (None,None)
@@ -90,11 +199,7 @@ def get_user_passwd_by_port(conffile, port):
             if str(port) == ln_ary[0]:
                 return (ln_ary[1],ln_ary[2])
     return (None, None)
-#}}}
-#{{{MGdb
 class MGdb(object):
-
-    
     def __init__(self, iphost="127.0.0.1", port=27017, username=None, password=None, force=False, debug=True):
         self._iphost = iphost
         self._port = port
@@ -241,12 +346,7 @@ class MGdb(object):
     def print_all_key_val(self):
         resobj = self._get_result()
         print json.dumps(resobj, indent=4)
-
-#}}}
-    
-
 def main():
-#{{{usage
     usage = "usage: %prog [options]\n Fetch mongodb status"
     parser = OptionParser(usage)
     
@@ -254,7 +354,7 @@ def main():
                       action="store_true", dest="is_list", default=False,  
                       help="if list all port")
 
-    parser.add_option("-H", "--host", action="store", dest="host", type="string", default='localhost', help="Connect to memcached host.")
+    parser.add_option("-H", "--host", action="store", dest="host", type="string", default='localhost', help="Connect to mongod host.")
 
     parser.add_option("-p", 
                       "--port", 
@@ -298,12 +398,10 @@ def main():
     parser.add_option("-a", "--all",  
                       action="store_true",dest="all", default=False,  
                       help="output all info")
-#}}}
     (options, args) = parser.parse_args()
     if 1 >= len(sys.argv):
         parser.print_help()
         return
-
     hostname = options.host
     port = options.port
     
@@ -316,9 +414,6 @@ def main():
         #print "Get (username=%s,password=%s) From Config File By port:%s" % (username, password, port)
 
     monitor_obj = MGdb(iphost=hostname, port=port, username=username, password=password, debug=options.debug, force=options.force)
-
-
-
     #################start
     port_list=[27017]
     if options.is_list == True:
@@ -329,6 +424,7 @@ def main():
         mg = MGdb(debug=False)
         mg.print_all_key_val()
         sys.exit(0)
+
     try:
         item = options.item
         item_ary = re.split("\.", item)
