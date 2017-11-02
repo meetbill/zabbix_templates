39555b584895302fc03ca0ce4211bfadb477bbf7
meetbill <meetbill@163.com>
2017-11-03 02:12:37 +0800

    update:template_raid 1.0.2

--- template_raid/README.md
+++ template_raid/README.md
@@ -4,6 +4,8 @@
 * [1 安装](#1-安装)
 * [2 检验](#2-检验)
 * [3 原理](#3-原理)
+* [4 版本](#4-版本)
+* [5 相关项目](#5-相关项目)
 
 <!-- vim-markdown-toc -->
 
@@ -32,3 +34,11 @@ install 中安装执行 install.sh
 
 * install 里面安装的程序是可以使 agent 采集到要监控的数据
 * templates 中包括了要监控机器要监控的指标及报警条件，导入后关联要监控的主机即可
+
+## 4 版本
+
+* v1.0.2 : 优化执行速度
+
+## 5 相关项目
+
+* raid 终端查看工具 [megacli_tui](https://github.com/BillWang139967/megacli_tui)
--- template_raid/install/conf/raid.py
+++ template_raid/install/conf/raid.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
+#!/usr/bin/python
+#coding=utf8
+"""
+# Author: meetbill
+# Created Time : 2017-11-03 00:54:10
+
+# File Name: raid.py
 # Description:
-#   This application is used to discovery the pyhsical disk by using the MegaCLI tool.
-#
-#
 
-
+"""
 import commands
 import os
 import sys
 import json
-from optparse import OptionParser
 
 
 MEGACLI_EXEC = '/opt/MegaRAID/MegaCli/MegaCli64'
@@ -24,52 +24,14 @@ MEC = 'Media Error Count'
 OEC = 'Other Error Count'
 PFC = 'Predictive Failure Count'
 PD_TYPE = 'PD Type'
-RAW_SIZE = 'Raw Size'
 FIRMWARE_STATE = 'Firmware state'
-INQUIRY_DATA = 'Inquiry Data'
 
 
-class Disk(object):
-#{{{ __init__
-    def __init__(self, dev_id, slot_number, mec, oec, pfc, pd_type,
-                 raw_size, firmware_state, inquiry_data):
-        self.dev_id = dev_id
-        self.slot_number = slot_number
-        # Media Error Count
-        self.mec = mec
-        # Other Error Count
-        self.oec = oec
-        # Predictive Failure Count
-        self.pfc = pfc
-        # PD Type
-        self.pd_type = pd_type
-        # Size
-        self.raw_size = raw_size
-        # Firmware State ("Failed", "Online, Spun Up", "Online, Spun Down", "Unconfigured(bad)", "Unconfigured(good), Spun down", "Hotspare, Spun down", "Hotspare, Spun up" or "not Online")
-        self.firmware_state = firmware_state
-        # Inquiry data
-        self.inquiry_data = inquiry_data
-#}}}
-#{{{ jsonfiy
-    def jsonfiy(self):
-        pass
-#}}}
-#{{{__str__
-    def __str__(self):
-        return '%s %s %s %s %s %s %s %s  %s' % (
-            self.dev_id, self.slot_number, self.mec, self.oec,
-            self.pfc, self.pd_type, self.raw_size, self.firmware_state,
-            self.inquiry_data
-        )
-#}}}
-
-def check_megacli(cli_path):
+def _check_megacli(cli_path):
     if not os.path.exists(cli_path) or not os.access(cli_path, os.X_OK):
         print 'MegaCLI is needed in %s with executable priviledge.' % (cli_path)
         sys.exit(1)
-
-
-def line_generator(string):
+def _line_generator(string):
     line = []
     for c in string:
         if c != '\n':
@@ -77,105 +39,101 @@ def line_generator(string):
         else:
             yield ''.join(line)
             line = []
-
-
-def get_value(line):
+def _get_value(line):
     return line.split(':')[1].strip()
-
-
-def make_disk_array(mega_output):
-    disk_array = []
-    for line in line_generator(mega_output):
+def _make_disk_dict(mega_output):
+    disk_dict_all = {}
+    for line in _line_generator(mega_output):
         if line.startswith(SLOT_NUMBER):
-            slot_number = get_value(line)
+            slot_number = _get_value(line)
         elif line.startswith(DEVICE_ID):
-            dev_id = get_value(line)
+            dev_id = _get_value(line)
         elif line.startswith(MEC):
-            mec = get_value(line)
+            mec = _get_value(line)
         elif line.startswith(OEC):
-            oec = get_value(line)
+            oec = _get_value(line)
         elif line.startswith(PFC):
-            pfc = get_value(line)
+            pfc = _get_value(line)
         elif line.startswith(PD_TYPE):
-            pd_type = get_value(line)
-        elif line.startswith(RAW_SIZE):
-            raw_size = get_value(line)
+            pd_type = _get_value(line)
         elif line.startswith(FIRMWARE_STATE):
-            fw_state = get_value(line)
-        elif line.startswith(INQUIRY_DATA):
-            inquiry_data = get_value(line)
+            fw_state = _get_value(line)
 
-            disk = Disk(dev_id, slot_number, mec, oec, pfc, pd_type,
-                        raw_size, fw_state, inquiry_data)
-            disk_array.append(disk)
-    return disk_array
+            device_dict = {}
+            device_dict["slot_number"] = slot_number
+            device_dict["dev_id"] = dev_id
+            device_dict["mec"] = mec
+            device_dict["oec"] = oec
+            device_dict["pfc"] = pfc
+            device_dict["pd_type"] = pd_type
+            device_dict["fw_state"] = fw_state
 
-
-def discovery_physical_disk(disk_array):
-    array = []
-    for d in disk_array:
-        disk = {}
-        disk['{#DISK_ID}'] = d.dev_id
-        array.append(disk)
-    return json.dumps({'data': array}, indent=4, separators=(',',':'))
-
-
-def count_media_error(disk_array, disk_id):
-    for disk in disk_array:
-        if int(disk.dev_id) == int(disk_id):
-            return disk.mec
-    return '-1'
-
-def count_other_error(disk_array, disk_id):
-    for disk in disk_array:
-        if int(disk.dev_id) == int(disk_id):
-            return disk.oec
-    return '-1'
-
-def count_predictive_error(disk_array, disk_id):
-    for disk in disk_array:
-        if int(disk.dev_id) == int(disk_id):
-            return disk.pfc
-    return '-1'
-
-
-def get_disk_array():
-    check_megacli(MEGACLI_EXEC)
+            disk_dict_all[dev_id]=device_dict
+    return disk_dict_all
+def _get_disk_dict():
+    _check_megacli(MEGACLI_EXEC)
     (status, output) = commands.getstatusoutput('%s %s' % (MEGACLI_EXEC, LIST_DISK_OPT))
     if status != 0:
         print 'Exec MegaCLI failed, please check the log.'
         sys.exit(1)
     #print output
-    disk_array = make_disk_array(output)
-    #print disk_array
-    return disk_array
-
-
-def init_option():
-    usage = """
+    disk_dict = _make_disk_dict(output)
+    return disk_dict
+def pd_discovery():
     """
-    parser = OptionParser(usage=usage, version="0.1")
-    return parser
-
-
-parser = init_option()
-
+    discovery_physical_disk
+    """
+    array = []
+    for dev_id in disk_dict_all:
+        disk = {}
+        disk['{#DISK_ID}'] = disk_dict_all[dev_id]["dev_id"]
+        disk['{#SLOT_NUMBER}'] = disk_dict_all[dev_id]["slot_number"]
+        array.append(disk)
+    return json.dumps({'data': array}, indent=4, separators=(',',':'))
+def media_error_count(disk_id):
+    try:
+        return disk_dict_all[disk_id]["mec"]
+    except:
+        return '-1'
+def other_error_count(disk_id):
+    try:
+        return disk_dict_all[disk_id]["oec"]
+    except:
+        return '-1'
+def predictive_error_count(disk_id):
+    try:
+        return disk_dict_all[disk_id]["pfc"]
+    except:
+        return '-1'
 
 if __name__ == '__main__':
-    (options, args) = parser.parse_args()
-
-    if len(args) < 1:
-        print parser.print_help()
-        sys.exit(1)
-
-    disk_array = get_disk_array()
-    #print disk_array
-    command = args.pop(0)
-    if command == 'pd_discovery':
-        print discovery_physical_disk(disk_array)
-    elif command == 'mec':
-        print count_media_error(disk_array, args.pop())
-    elif command == 'oec':
-        print count_other_error(disk_array, args.pop())
-    elif command == 'pfc':
-        print count_predictive_error(disk_array, args.pop())
+    import sys, inspect
+    if len(sys.argv) < 2:
+        print "Usage(1.0.2):"
+        for k, v in sorted(globals().items(), key=lambda item: item[0]):
+            if inspect.isfunction(v) and k[0] != "_":
+                args, __, __, defaults = inspect.getargspec(v)
+                if defaults:
+                    print sys.argv[0], k, str(args[:-len(defaults)])[1:-1].replace(",", ""), \
+                          str(["%s=%s" % (a, b) for a, b in zip(args[-len(defaults):], defaults)])[1:-1].replace(",", "")
+                else:
+                    print sys.argv[0], k, str(v.func_code.co_varnames[:v.func_code.co_argcount])[1:-1].replace(",", "")
+        sys.exit(-1)
+    else:
+        disk_dict_all = _get_disk_dict()
+        func = eval(sys.argv[1])
+        args = sys.argv[2:]
+        try:
+            r = func(*args)
+            print r
+        except Exception, e:
+            print "Usage:"
+            print "\t", "python %s" % sys.argv[1], str(func.func_code.co_varnames[:func.func_code.co_argcount])[1:-1].replace(",", "")
+            if func.func_doc:
+                print "\n".join(["\t\t" + line.strip() for line in func.func_doc.strip().split("\n")])
+            print e
+            r = -1
+            import traceback
+            traceback.print_exc()
+        if isinstance(r, int):
+            sys.exit(r)
